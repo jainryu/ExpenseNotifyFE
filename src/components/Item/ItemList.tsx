02adf436a01adb30aac5be4fd6ca47f804c4d62d
jainryu <79353841+jainryu@users.noreply.github.com>
2025-07-22 23:30:06 -0700

    add react query

--- src/components/Item/ItemList.tsx
+++ src/components/Item/ItemList.tsx
@@ -6,33 +6,51 @@ import { useState } from "react";
 import SaveButton from "../Button/SaveButton";
 import CreateButton from "../Button/CreateButton";
 import ExpenseModal from "../ExpenseModal/ExpenseModal";
+import axios from "axios";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 
 const initialItems: ItemProps[] = [
-    {
-        date: new Date('2023-10-01'),
-        title: 'Lunch with Friends',
-        description: 'Lunch at the new Italian restaurant downtown.',
-        amount: 45.00,
-        splitStatus: true
-    },
-    {
-        date: new Date('2023-10-02'),
-        title: 'Grocery Shopping',
-        description: 'Weekly grocery shopping at the local market.',
-        amount: 120.50,
-        splitStatus: false
-    },
-    {
-        date: new Date('2023-10-03'),
-        title: 'Movie Night',
-        description: 'Tickets for the latest blockbuster movie.',
-        amount: 30.00,
-        splitStatus: true
-    }
+  {
+    date: new Date('2023-10-01'),
+    title: 'Lunch with Friends',
+    description: 'Lunch at the new Italian restaurant downtown.',
+    amount: 45.00,
+    splitStatus: true
+  },
+  {
+    date: new Date('2023-10-02'),
+    title: 'Grocery Shopping',
+    description: 'Weekly grocery shopping at the local market.',
+    amount: 120.50,
+    splitStatus: false
+  },
+  {
+    date: new Date('2023-10-03'),
+    title: 'Movie Night',
+    description: 'Tickets for the latest blockbuster movie.',
+    amount: 30.00,
+    splitStatus: true
+  }
 ];
 
+const fetchItems = async () => {
+  const { data } = await axios.get('/api/items');
+  return data;
+}
+
+
 const ItemList = () => {
+  const queryClient = useQueryClient()
+
+  // const { data, isLoading, error } = useQuery({
+  //   queryKey: ['items'],
+  //   queryFn: fetchItems,
+  // })
+
+  // if (isLoading) return <p>Loading...</p>;
+  // if (error) return <p>Error loading</p>;
+
   const [items, setItems] = useState<ItemProps[]>(initialItems);
   const [showModal, setShowModal] = useState(false);
 
@@ -48,18 +66,18 @@ const ItemList = () => {
 
   const pendingItems = items.filter(item => !item.splitStatus);
   const completedItems = items.filter(item => item.splitStatus);
-  
+
   return (
     <div className="p-4 max-w-2xl mx-auto min-h-[300px]">
-        <div className="button-group">
-          <CreateButton onClick={() => setShowModal(true)}/>
-          {showModal && <ExpenseModal onClose={() => setShowModal(false)} onCreate={handleCreate} />}
-          <SaveButton />
-        </div>
+      <div className="button-group">
+        <CreateButton onClick={() => setShowModal(true)} />
+        {showModal && <ExpenseModal onClose={() => setShowModal(false)} onCreate={handleCreate} />}
+        <SaveButton />
+      </div>
 
-        <ItemHeader/>
-        <ItemListAccordion title="Pending Expenses" defaultTitle="No Pending Expenses" defaultOpen={true} onSplitChange={handleSplitChange} fullList={items} expenses={pendingItems}/>
-        <ItemListAccordion title="Completed Expenses" defaultTitle="No Completed Expenses" onSplitChange={handleSplitChange} fullList={items} expenses={completedItems}/>
+      <ItemHeader />
+      <ItemListAccordion title="Pending Expenses" defaultTitle="No Pending Expenses" defaultOpen={true} onSplitChange={handleSplitChange} fullList={items} expenses={pendingItems} />
+      <ItemListAccordion title="Completed Expenses" defaultTitle="No Completed Expenses" onSplitChange={handleSplitChange} fullList={items} expenses={completedItems} />
     </div>
   );
 }
